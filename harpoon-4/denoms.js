6156e63ebddf4dccaf867cb378e9568f1f5a17c6
PFC <81114960+PFC-developer@users.noreply.github.com>
2024-03-12 15:33:16 -0500

    Create denoms.js

---
+++ harpoon-4/denoms.js
@@ -0,0 +1,11 @@
+[
+  {"denom":"factory/kujira1r85reqy6h0lu02vyz0hnzhv5whsns55gdt4w0d7ft87utzk7u0wqr4ssll/uusk",
+   "code":"USK",
+   "decimals":6
+  },
+  
+  {"denom":"factory/kujira1hf3898lecj8lawxq8nwqczegrla9denzfkx4asjg0q27cyes44sq68gvc9/ampKUJI",
+   "code":"ampKuji",
+   "decimals":6
+  }
+]
